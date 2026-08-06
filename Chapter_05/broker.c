c31742e95312bf44fef61f1c1f2c4961b5583339
Ankur <singhankurdev1@gmail.com>
2026-08-06 09:18:05 +0530

    Modification

--- Chapter_05/broker.c
+++ Chapter_05/broker.c
@@ -1,25 +1,22 @@
-// Calculating a Broker's Commission
 #include <stdio.h>
 int main(void)
-{	float commission, value;
-	printf("Enter the value of trade: ");
-	scanf("%f", &value);
-	
-	if (value < 2500.00f)
-	    commission = 30.00f + 0.17f * value;
-	else if (value < 6250.00f)
-	    commission = 56.00f + .0066f * value;
-	else if (value < 2000.00f)
-	    commission = 76.00f + .0034f * value;
-	else if (value < 5000.00f)
-	    commission = 100.00f + .0022f * value;
-	else if (value < 500000.00f)
-	    commission = 155.00f + .0011f * value;
-	else 
-	    commission = 255.00f + .0009f * value;
-	if (commission < 39.00f)
-	    commission = 39.00f;
-	printf("Commission: $%.2f\n", commission);
-	
-	return 0;
+{	int shares;
+float price, value;
+float original_commission, rival_commission;
+
+printf("Enter number of shares: ");
+scanf("%d", &shares);
+
+printf("Enter price per share: ");
+scanf("%f", &price);
+
+value = shares * price;
+
+/* original broker's commission calculation */
+
+/* rival broker's commission calculation */
+
+printf("Original broker's commission: $%.2f\n", original_commission);
+printf("Rival broker's commission: $%.2f\n", rival_commission);
+return 0;
 }
