c1c54d2642695c001b38377a897a68517d515091
Ankur <singhankurdev1@gmail.com>
2026-08-27 20:40:54 +0530

    Chapter_07 Initial Commits

---
+++ Chapter_07/sum2.c
@@ -0,0 +1,15 @@
+//Sums a series of numbers (using long variables)
+#include <stdio.h>
+int main(void)
+{	
+	long n, sum = 0;
+	printf("This program sums a series of integers.\n");
+	printf("Enter integers (0 to terminate): ");
+	scanf("%d", &n);
+	while (n != 0) {
+		sum += n;
+		scanf("%ld", &n);
+	}
+	printf("The sum is : %d\n ", sum);
+	return 0;
+}
